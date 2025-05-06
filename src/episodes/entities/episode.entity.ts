d46cfdf7462e5ff7ebc0a11573042efbbb36ae44
Patrick Simões <patricksimoes25@gmail.com>
2025-05-05 21:50:56 -0300

    feat: db episodes entity created

--- src/episodes/entities/episode.entity.ts
+++ src/episodes/entities/episode.entity.ts
@@ -1,1 +1,49 @@
-export class Episode {}
+import {
+  Table,
+  Column,
+  Model,
+  DataType,
+  PrimaryKey,
+  Default,
+  AllowNull,
+} from 'sequelize-typescript';
+
+@Table({
+  tableName: 'episodes',
+  underscored: true,
+  timestamps: true,
+})
+export class Episode extends Model<Episode> {
+  @PrimaryKey
+  @Default(DataType.UUIDV4)
+  @Column(DataType.UUID)
+  declare id: string;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  title!: string;
+
+  @AllowNull(true)
+  @Column(DataType.TEXT)
+  description?: string;
+
+  @AllowNull(false)
+  @Column(DataType.INTEGER)
+  season!: number;
+
+  @AllowNull(false)
+  @Column({ type: DataType.INTEGER, field: 'episode_number' })
+  episodeNumber!: number;
+
+  @AllowNull(true)
+  @Column({ type: DataType.DATE, field: 'air_date' })
+  airDate?: Date;
+
+  @AllowNull(true)
+  @Column({ type: DataType.INTEGER, comment: 'duration in minutes' })
+  duration?: number;
+
+  @AllowNull(true)
+  @Column({ type: DataType.FLOAT, comment: 'average rating' })
+  rating?: number;
+}
--- src/episodes/episodes.module.ts
+++ src/episodes/episodes.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EpisodesService } from './episodes.service';
 import { EpisodesController } from './episodes.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Episode } from './entities/episode.entity';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Episode])],
   controllers: [EpisodesController],
   providers: [EpisodesService],
 })
