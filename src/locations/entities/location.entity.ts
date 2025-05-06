0eeeb7e886cd09488332a351a901a65d67ff7f65
Patrick Simões <patricksimoes25@gmail.com>
2025-05-05 22:17:48 -0300

    feat: implement Locations entity

--- src/locations/entities/location.entity.ts
+++ src/locations/entities/location.entity.ts
@@ -1,1 +1,43 @@
-export class Location {}
+import {
+  Table,
+  Column,
+  Model,
+  DataType,
+  PrimaryKey,
+  Default,
+  AllowNull,
+} from 'sequelize-typescript';
+
+@Table({
+  tableName: 'locations',
+  underscored: true,
+  timestamps: true,
+})
+export class Location extends Model<Location> {
+  @PrimaryKey
+  @Default(DataType.UUIDV4)
+  @Column(DataType.UUID)
+  declare id: string;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  name: string;
+
+  @AllowNull(true)
+  @Column(DataType.TEXT)
+  description?: string;
+
+  @Column
+  city: string;
+
+  @Column
+  country: string;
+
+  @AllowNull(true)
+  @Column(DataType.FLOAT)
+  latitude?: number;
+
+  @AllowNull(true)
+  @Column(DataType.FLOAT)
+  longitude?: number;
+}
--- src/locations/locations.module.ts
+++ src/locations/locations.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LocationsService } from './locations.service';
 import { LocationsController } from './locations.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Location } from './entities/location.entity';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Location])],
   controllers: [LocationsController],
   providers: [LocationsService],
 })
