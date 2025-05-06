f9b223d6a7d6c34c8205e22158291e61a75ec420
Patrick Simões <patricksimoes25@gmail.com>
2025-05-05 21:58:23 -0300

    feat(gadgets): implement Gadget entity

--- src/gadgets/entities/gadget.entity.ts
+++ src/gadgets/entities/gadget.entity.ts
@@ -1,1 +1,45 @@
-export class Gadget {}
+import {
+  Table,
+  Column,
+  Model,
+  DataType,
+  PrimaryKey,
+  Default,
+  AllowNull,
+} from 'sequelize-typescript';
+
+@Table({
+  tableName: 'gadgets',
+  underscored: true,
+  timestamps: true,
+})
+export class Gadget extends Model<Gadget> {
+  @PrimaryKey
+  @Default(DataType.UUIDV4)
+  @Column(DataType.UUID)
+  declare id: string;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  name: string;
+
+  @AllowNull(true)
+  @Column(DataType.TEXT)
+  description?: string;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  model: string;
+
+  @AllowNull(true)
+  @Column(DataType.STRING)
+  manufacturer?: string;
+
+  @AllowNull(true)
+  @Column({ type: DataType.STRING })
+  serialNumber?: string;
+
+  @AllowNull(true)
+  @Column({ type: DataType.DATE })
+  releaseDate?: Date;
+}
--- src/gadgets/gadgets.module.ts
+++ src/gadgets/gadgets.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GadgetsService } from './gadgets.service';
 import { GadgetsController } from './gadgets.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Gadget } from './entities/gadget.entity';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Gadget])],
   controllers: [GadgetsController],
   providers: [GadgetsService],
 })
