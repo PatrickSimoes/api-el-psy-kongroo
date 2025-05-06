3618d3519b4f6f84f081df5a510ef616c9e2ce9d
Patrick Simões <patricksimoes25@gmail.com>
2025-05-05 22:11:19 -0300

    feat: implement Organizations entity

--- src/organizations/entities/organization.entity.ts
+++ src/organizations/entities/organization.entity.ts
@@ -1,1 +1,33 @@
-export class Organization {}
+import {
+  Table,
+  Column,
+  Model,
+  DataType,
+  PrimaryKey,
+  Default,
+  AllowNull,
+} from 'sequelize-typescript';
+
+@Table({
+  tableName: 'organizations',
+  underscored: true,
+  timestamps: true,
+})
+export class Organization extends Model<Organization> {
+  @PrimaryKey
+  @Default(DataType.UUIDV4)
+  @Column(DataType.UUID)
+  declare id: string;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  name: string;
+
+  @AllowNull(true)
+  @Column(DataType.TEXT)
+  description?: string;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  address: string;
+}
--- src/organizations/organizations.module.ts
+++ src/organizations/organizations.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OrganizationsService } from './organizations.service';
 import { OrganizationsController } from './organizations.controller';
+import { Organization } from './entities/organization.entity';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Organization])],
   controllers: [OrganizationsController],
   providers: [OrganizationsService],
 })
