6970695092cc7f69ff6166016b761168a1402942
Patrick Simões <patricksimoes25@gmail.com>
2025-05-05 22:07:30 -0300

    feat: implement Wordlines entity

--- src/worldlines/entities/worldline.entity.ts
+++ src/worldlines/entities/worldline.entity.ts
@@ -1,1 +1,33 @@
-export class Worldline {}
+import {
+  Table,
+  Column,
+  Model,
+  DataType,
+  PrimaryKey,
+  Default,
+  AllowNull,
+} from 'sequelize-typescript';
+
+@Table({
+  tableName: 'wordlines',
+  underscored: true,
+  timestamps: true,
+})
+export class Wordline extends Model<Wordline> {
+  @PrimaryKey
+  @Default(DataType.UUIDV4)
+  @Column(DataType.UUID)
+  declare id: string;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  word: string;
+
+  @AllowNull(false)
+  @Column({ type: DataType.INTEGER, field: 'line_number' })
+  lineNumber!: number;
+
+  @AllowNull(true)
+  @Column({ type: DataType.INTEGER, comment: 'nth occurrence in the same line' })
+  occurrence?: number;
+}
--- src/worldlines/worldlines.module.ts
+++ src/worldlines/worldlines.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WorldlinesService } from './worldlines.service';
 import { WorldlinesController } from './worldlines.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Wordline } from './entities/worldline.entity';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Wordline])],
   controllers: [WorldlinesController],
   providers: [WorldlinesService],
 })
