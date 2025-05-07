bba610f68a1a614a307c4e9a4a197c6619521904
Patrick Simões <patricksimoes25@gmail.com>
2025-05-06 21:39:26 -0300

    fix: adjustment Wordline module

--- src/worldlines/worldlines.module.ts
+++ src/worldlines/worldlines.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { WorldlinesService } from './worldlines.service';
 import { WorldlinesController } from './worldlines.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { Wordline } from './entities/worldline.entity';
+import { Worldline } from './entities/worldline.entity';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Wordline])],
+  imports: [SequelizeModule.forFeature([Worldline])],
   controllers: [WorldlinesController],
   providers: [WorldlinesService],
 })
