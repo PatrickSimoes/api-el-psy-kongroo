c4275b623e465e8aaa948857efa447aaee3c461d
Patrick Simões <patricksimoes25@gmail.com>
2025-05-06 21:12:36 -0300

    feat: charactersLocations implemented

--- src/character_location/character_location.module.ts
+++ src/character_location/character_location.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CharacterLocationService } from './character_location.service';
 import { CharacterLocationController } from './character_location.controller';
+import { CharacterLocation } from './entities/character_location.entity';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
+  imports: [SequelizeModule.forFeature([CharacterLocation])],
   controllers: [CharacterLocationController],
   providers: [CharacterLocationService],
 })
--- src/character_location/entities/character_location.entity.ts
+++ src/character_location/entities/character_location.entity.ts
@@ -1,1 +1,33 @@
-export class CharacterLocation {}
+import {
+  Column,
+  DataType,
+  Default,
+  ForeignKey,
+  Model,
+  PrimaryKey,
+  Table,
+} from 'sequelize-typescript';
+import { Character } from 'src/characters/entities/character.entity';
+import { Location } from 'src/locations/entities/location.entity';
+
+@Table
+export class CharacterLocation extends Model {
+  @PrimaryKey
+  @Default(DataType.UUIDV4)
+  @Column
+  declare id: string;
+
+  @ForeignKey(() => Character)
+  @Column({
+    type: DataType.UUIDV4,
+    allowNull: false,
+  })
+  characterId: string;
+
+  @ForeignKey(() => Location)
+  @Column({
+    type: DataType.UUIDV4,
+    allowNull: false,
+  })
+  locationId: string;
+}
