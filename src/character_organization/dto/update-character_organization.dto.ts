0e5227b85ba8f7d206b1ab289dc46d7f930b4efa
Patrick Simões <patricksimoes25@gmail.com>
2025-05-07 22:04:11 -0300

    feat: implments dto characterOrganization

--- src/character_organization/dto/update-character_organization.dto.ts
+++ src/character_organization/dto/update-character_organization.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateCharacterOrganizationDto } from './create-character_organization.dto';
 
 export class UpdateCharacterOrganizationDto extends PartialType(CreateCharacterOrganizationDto) {}
