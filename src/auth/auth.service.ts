2113c2f536a0471c8dfeab317cb5fc5608b6e04c
Patrick Simões <patricksimoes25@gmail.com>
2025-05-11 16:44:20 -0300

    feat: implements base auth

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async signIn(email: string, password: string): Promise<any> {
+    const user = await this.usersService.findOneByEmail(email);
+
+    if (user?.password !== password) {
+      throw new UnauthorizedException();
+    }
+
+    // TODO: Generate a JWT and return it here
+
+    return { status: 200, user };
+  }
+}
